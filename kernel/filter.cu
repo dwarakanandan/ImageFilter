ac899ba2f9f7103eba2e1306dc541b4cd1276095
Ashwin Arora <arora.ashwin95@gmail.com>
2021-01-06 03:17:16 +0100

    Added Gamma and Scale, Added Restrict to Splat

--- kernel/filter.cu
+++ kernel/filter.cu
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #define BLOCK_SIZE 32
 #define GAUSSIAN_RANGE 3
+#define COMPUTE_TYPE double
 __device__ double gaussian(double x0, double size)
 {
 	double d = x0 / size;
@@ -185,8 +186,26 @@ struct CudaAtomicAdd <double>
 };
 
 
+__constant__ COMPUTE_TYPE r_weight_const;
+
 template <typename T>
-__global__ void GaussianSplatSTX_GPU_kernel(T* target, T* source, int width, int height, T scale, T d, BoundaryCondition boundary, bool add) {
+void setConstantMemWeight(T d,T scale){
+	T weight = gaussian(d, scale);
+	int bound = (int)floor(GAUSSIAN_RANGE * scale);
+		for (int x0 = 1; x0 <= bound; x0++)
+		{
+			T ga = gaussian(-x0 + d, scale);
+			T gb = gaussian(x0 + d, scale);
+			weight += ga + gb;
+		}
+	T r_weight = 1.0f / weight;
+	cudaMemcpyToSymbol(r_weight_const,&r_weight,sizeof(T));
+	std::cout<<"ConstantMem Done";
+}
+
+
+template <typename T>
+__global__ void GaussianSplatSTX_GPU_kernel(T* __restrict__ target,const T* __restrict__ source, int width, int height, T scale, T d, BoundaryCondition boundary, bool add) {
     int bound = (int)floor(GAUSSIAN_RANGE * scale);
     
     int x = threadIdx.x + blockIdx.x * blockDim.x;
@@ -218,11 +237,11 @@ __global__ void GaussianSplatSTX_GPU_kernel(T* target, T* source, int width, int
 					T gb = gaussian(x0 + d, scale);
 					int xa = x - x0;
 					int xb = x + x0;
-					if (xa >= 0) target[xa + y * width] += t * ga;
-					if (xb < width) target[xb + y * width] += t * gb;
+					if (xa >= 0) caa.AtomicAdd(&target[xa + y * width],t * ga);
+					if (xb < width) caa.AtomicAdd(&target[xb + y * width],t * gb);
 				}
 				T g = gaussian(d, scale);
-				target[x + y * width] += t * g;
+				caa.AtomicAdd(&target[x + y * width],t * g);
 			}
 		// }
 	}
@@ -263,9 +282,8 @@ __global__ void GaussianSplatSTX_GPU_kernel(T* target, T* source, int width, int
 	}
 }
 
-
 template <typename T>
-void GaussianSplatSTX_GPU(T* target, T* source, int width, int height, T scale, T d, BoundaryCondition boundary, bool add) {
+void GaussianSplatSTX_GPU(T* target,const T* source, int width, int height, T scale, T d, BoundaryCondition boundary, bool add) {
     dim3 block_size;
 	block_size.x = BLOCK_SIZE;
 	block_size.y = BLOCK_SIZE;
@@ -275,15 +293,13 @@ void GaussianSplatSTX_GPU(T* target, T* source, int width, int height, T scale, 
 	grid_size.x = (width + block_size.x - 1) / block_size.x;
 	grid_size.y = (height + block_size.y - 1) / block_size.y;
     grid_size.z = 1;
-    
+    //setConstantMemWeight((T)d,(T)scale);
     GaussianSplatSTX_GPU_kernel<<<grid_size, block_size>>>((T *)target, (T *)source, width, height, (T)scale, (T)d, boundary, add);
     CHECK_LAUNCH_ERROR();
 }
 
-
-
 template <typename T>
-__global__ void GaussianSplatSTY_GPU_kernel(T* target, T* source, int width, int height, T scale, T d, BoundaryCondition boundary, bool add) {
+__global__ void GaussianSplatSTY_GPU_kernel(T* __restrict__ target,const T* __restrict__ source, int width, int height, T scale, T d, BoundaryCondition boundary, bool add) {
     int bound = (int)floor(GAUSSIAN_RANGE * scale);
     
     int x = threadIdx.x + blockIdx.x * blockDim.x;
@@ -366,10 +382,8 @@ __global__ void GaussianSplatSTY_GPU_kernel(T* target, T* source, int width, int
     
 }
 
-
-
 template <typename T>
-void GaussianSplatSTY_GPU(T* target, T* source, int width, int height, T scale, T d, BoundaryCondition boundary, bool add) {
+void GaussianSplatSTY_GPU(T* target,const T* source, int width, int height, T scale, T d, BoundaryCondition boundary, bool add) {
     dim3 block_size;
 	block_size.x = BLOCK_SIZE;
 	block_size.y = BLOCK_SIZE;
@@ -384,8 +398,118 @@ void GaussianSplatSTY_GPU(T* target, T* source, int width, int height, T scale, 
 	CHECK_LAUNCH_ERROR();
 }
 
-template void GaussianSplatSTY_GPU<float>(float* target, float* source, int width, int height, float scale, float d, BoundaryCondition boundary, bool add);
-template void GaussianSplatSTY_GPU<double>(double* target, double* source, int width, int height, double scale, double d, BoundaryCondition boundary, bool add);
+template void GaussianSplatSTY_GPU<float>(float* target,const float* source, int width, int height, float scale, float d, BoundaryCondition boundary, bool add);
+template void GaussianSplatSTY_GPU<double>(double* target,const double* source, int width, int height, double scale, double d, BoundaryCondition boundary, bool add);
 
-template void GaussianSplatSTX_GPU<float>(float* target, float* source, int width, int height, float scale, float d, BoundaryCondition boundary, bool add);
-template void GaussianSplatSTX_GPU<double>(double* target, double* source, int width, int height, double scale, double d, BoundaryCondition boundary, bool add);
+template void GaussianSplatSTX_GPU<float>(float* target,const float* source, int width, int height, float scale, float d, BoundaryCondition boundary, bool add);
+template void GaussianSplatSTX_GPU<double>(double* target,const double* source, int width, int height, double scale, double d, BoundaryCondition boundary, bool add);
+
+template <typename T>
+__global__ void Gamma_GPU_Kernel(T* target,int target_width,T gamma)
+{
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	T v = target[x + y * target_width];
+	if(v>0.0f) target[x + y * target_width] = pow(v, T(1)/gamma);
+}
+
+
+
+template <typename T> 
+void Gamma_GPU(T* target, int target_width,int target_height,T gamma)
+{
+	dim3 block_size;
+	block_size.x = BLOCK_SIZE;
+	block_size.y = BLOCK_SIZE;
+	block_size.z = 1;
+
+	dim3 grid_size;
+	grid_size.x = (target_width + block_size.x - 1) / block_size.x;
+	grid_size.y = (target_height + block_size.y - 1) / block_size.y;
+	grid_size.z = 1;
+	Gamma_GPU_Kernel<<<grid_size,block_size>>>((T*)target,target_width,(T)gamma);
+	CHECK_LAUNCH_ERROR(); 
+}
+
+
+
+template void Gamma_GPU<float>(float* target,  int target_width, int target_height,float gamma);
+template void Gamma_GPU<double>(double* target,  int target_width, int target_height,double gamma);
+
+
+
+
+template <typename T> 
+__global__ void ScaleFast_GPU_Kernel(T* target, T* source, int target_width, int target_height, int source_width,  int source_height)
+{
+#if 1
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	// std::cout << "Fast scaling image " << source_width << "x" << source_height << " -> " << m_width << "x" << m_height << std::endl;
+	if (source_width > target_width)
+	{
+		unsigned int y0 = _min(y << 1, source_height - 1);
+		unsigned int y1 = _min(y0 + 1, source_height - 1);
+		unsigned int x0 = _min(x << 1, source_width - 1);
+		unsigned int x1 = _min(x0 + 1, source_width - 1);
+		T s = 0.25f * (source[x0 + y0*source_width] + source[x1 + y0*source_width] + source[x0 + y1*source_width] + source[x1 + y1*source_width]);
+		// SetValue(x, y, c, s);
+		target[x+y*target_width] = s;
+	}
+	else
+	{
+		unsigned int y0 = _min(y >> 1, source_height - 1);
+		unsigned int x0 = _min(x >> 1, source_width - 1);
+		// SetValue(x, y, c, source.GetValue(x0, y0, c));
+		target[x+y*target_width] = source[x0 + y0*source_width];
+	}
+#else
+	T scale_x = (T)(source_width) / (T)(target_width);
+	T scale_y = (T)(source_height) / (T)(m_height);
+
+	if (scale_x > 1.0f) scale_x = ceilf(scale_x);
+	else if (scale_x < 1.0f) scale_x = 1.0f / ceilf(1.0f / scale_x);
+	if (scale_y > 1.0f) scale_y = ceilf(scale_y);
+	else if (scale_y < 1.0f) scale_y = 1.0f / ceilf(1.0f / scale_y);
+
+	// unsigned int comp = source.GetComponents();
+	// SetComponents(comp);
+	// SetMin(0.0f);
+	// SetMax(1.0f);
+	// std::cout << "Fast scaling image with scale " << scale_x << ", " << scale_y << " (" << source_width << "x" << source_height << ") -> (" << target_width << "x" << m_height << ")" << std::endl;
+			T ys = _max(0.0f, _min(((T)y + 0.5f) * scale_y - 0.5f, source_height - 1.0f));
+			int y0 = (int)floorf(ys);
+			T yw = ys - y0;
+			for (unsigned int x = 0; x < target_width; x++)
+			{
+				T xs = _max(0.0f, _min(((T)x + 0.5f) * scale_x - 0.5f, source_width - 1.0f));
+				int x0 = (int)floorf(xs);
+				T xw = xs - x0;
+				T s = source[x0 + y0*source_width] * (1.0f - xw) * (1.0f - yw);
+				if (x0 + 1 < (int)source_width) s += source[(x0+1) + y0*source_width] * xw * (1.0f - yw);
+				if (y0 + 1 < (int)source_height) s += source[x0 + (y0+1)*source_width] * (1.0f - xw) * yw;
+				if ((x0 + 1 < (int)source_width) && (y0 + 1 < (int)source_height)) s += source[(x0+1) + (y0+1)*source_width] * xw * yw;
+				// SetValue(x, y, c, s);
+				target[x+y*target_width] = s;
+			}
+#endif
+}
+
+template <typename T>
+void ScaleFast_GPU(T* target, T* source, int target_width, int target_height,  int source_width,  int source_height)
+{
+	dim3 block_size;
+	block_size.x = BLOCK_SIZE;
+	block_size.y = BLOCK_SIZE;
+	block_size.z = 1;
+
+	dim3 grid_size;
+	grid_size.x = (target_width + block_size.x - 1) / block_size.x;
+	grid_size.y = (target_height + block_size.y - 1) / block_size.y;
+	grid_size.z = 1;
+	ScaleFast_GPU_Kernel<<<grid_size,block_size>>>((T*)target,(T*)source,target_width,target_height,source_width,source_height);
+	CHECK_LAUNCH_ERROR();
+}
+
+template void ScaleFast_GPU<float>(float* target,float* source, int target_width, int target_height, int source_width, int source_height);
+template void ScaleFast_GPU<double>(double* target, double* source, int target_width, int target_height, int source_width, int source_height);
--- kernel/filter.h
+++ kernel/filter.h
@@ -7,6 +7,10 @@ template <typename T> void GaussianFilterSTX_GPU(T* target, T* source, int width
 
 template <typename T> void GaussianFilterSTY_GPU(T* target, T* source, int width, int height, T scale, T d, BoundaryCondition boundary, bool add);
 
-template <typename T> void GaussianSplatSTX_GPU(T* target, T* source, int width, int height, T scale, T d, BoundaryCondition boundary, bool add);
+template <typename T> void GaussianSplatSTX_GPU(T* target,const T* source, int width, int height, T scale, T d, BoundaryCondition boundary, bool add);
 
-template <typename T> void GaussianSplatSTY_GPU(T *target, T *source, int width, int height, T scale, T d, BoundaryCondition boundary, bool add);
+template <typename T> void GaussianSplatSTY_GPU(T *target,const T *source, int width, int height, T scale, T d, BoundaryCondition boundary, bool add);
+
+template <typename T> void Gamma_GPU(T* target, int target_width,int target_height,T gamma);
+
+template <typename T> void ScaleFast_GPU(T* target,T* source, int target_width,int target_height, int source_width, int source_height);
